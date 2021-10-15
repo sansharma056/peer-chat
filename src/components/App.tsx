9e2399ae3187a5b8632f51fa336e668ccc3221d2
Sankalp Sharma <sansharma056@gmail.com>
2021-10-15 20:17:38 +0530

    added react router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Room from "./Room";
+import Home from "./Home";
+
 const App = () => {
-	return <h1 className="flex">go!</h1>;
+	return (
+		<Router>
+			<Switch>
+				<Route path="/:id">
+					<Room />
+				</Route>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Switch>
+		</Router>
+	);
 };
 
 export default App;
