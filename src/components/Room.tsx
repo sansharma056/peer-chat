a22649a457176d753db6e279afadcd577cd34ea4
Sankalp Sharma <sansharma056@gmail.com>
2021-10-20 23:03:54 +0530

    added styes, buttons, video

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,5 +1,7 @@
+import { useRef } from "react";
 import { useParams, Link } from "react-router-dom";
 import DocumentTitle from "react-document-title";
+import Button from "./Button";
 
 type RoomParams = {
 	id: string;
@@ -7,12 +9,54 @@ type RoomParams = {
 
 const Room = () => {
 	const { id } = useParams<RoomParams>();
+	const videoRef = useRef<HTMLVideoElement>(null);
+
+	async function startCapture(): Promise<MediaStream | null> {
+		let captureStream: MediaStream | null = null;
+
+		try {
+			captureStream = await navigator.mediaDevices.getDisplayMedia();
+		} catch (error) {
+			console.error(error);
+		}
+
+		return captureStream;
+	}
+
+	async function handleScreenShare() {
+		console.log("screen shared");
+
+		const captureStream = await startCapture();
+
+		if (videoRef.current) {
+			videoRef.current.srcObject = captureStream;
+			console.log(captureStream);
+		}
+	}
+
+	function handleMicrophoneShare() {
+		console.log("microphone shared");
+	}
 
 	return (
 		<DocumentTitle title="PeerChat - Room">
-			<div>
-				<h1>room id: {id}</h1>
-				<Link to="/">Go back</Link>
+			<div className="p-2.5 min-h-screen flex flex-col items-center">
+				<div className="h-2/3 w-11/12 flex flex-col mt-10 justify-around">
+					<h1 className="w-max">room id: {id}</h1>
+
+					<Link className="w-max hover:underline block" to="/">
+						Go back
+					</Link>
+
+					<div className="flex mt-2 justify-around">
+						<Button value="toggle share screen" onClick={handleScreenShare} />
+						<Button value="toggle microphone" onClick={handleMicrophoneShare} />
+					</div>
+				</div>
+
+				<div className="flex flex-grow justify-center mt-10 bg-gray-100 w-11/12">
+					<video className="w-4/5" ref={videoRef}></video>
+				</div>
 			</div>
 		</DocumentTitle>
 	);
