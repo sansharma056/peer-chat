437d71bc432c83449aae5cc20d5e03c9c4c74002
Sankalp Sharma <sansharma056@gmail.com>
2021-11-03 23:18:04 +0530

    modified tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
+	mode: "JIT",
 	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./src/index.html"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
 	},
 	variants: {
-		extend: {},
+		extend: {
+			backgroundColor: ["active"],
+		},
 	},
 	plugins: [],
 };
